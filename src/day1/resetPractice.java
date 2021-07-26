7ebe900cbf335ec4a80162022167e75359ee64d4
B23-User <ikramazim@hotmail.com>
2021-07-26 12:52:49 -0400

    Test case all is done

--- src/day1/resetPractice.java
+++ src/day1/resetPractice.java
@@ -4,4 +4,7 @@ public class resetPractice {
     // part 1 is done
     // part 2 is done
     // part 3 is done
+    // part 4 is done
+    // part 5 is done
+    // Mixed/soft reset remove the commit from history, but kept the local changes. Did part 4 part 5 and I am done in one commit
 }
