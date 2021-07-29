e43399fd161e2258fa17f85ddd43e07db180538a
B23-User <ikramazim@hotmail.com>
2021-07-29 15:52:28 -0400

    Watching until 5 minutes

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ListPractice {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<>();
+        //List<String> list = new ArrayList<>();
         // adding one item
         // printing
     }
