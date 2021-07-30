b45cfcb6002c6ed4bee97e59af7a45ff814a43d3
B23-User <ikramazim@hotmail.com>
2021-07-30 11:33:08 -0400

    Started tc001

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -1,12 +1,12 @@
 package day4;
 
 public class Conflict {
-//<<<<<<< HEAD
+
     // this is master branch content
-//=======
+
      //   i can type anything i want
 
     // this is the collection branch content
-//>>>>>>> collection
+
     // this is the change collection2  branch does not know about
 }
